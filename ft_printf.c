2582327986c87f50577bade0cf69b2d137d43450
jsegovia <jorge.segovia.gil@gmail.com>
2022-03-04 11:04:23 +0100

    Initial commits

---
+++ ft_printf.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsegovia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/04 10:17:43 by jsegovia          #+#    #+#             */
+/*   Updated: 2022/03/04 10:17:45 by jsegovia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Requirements:
+• Don’t implement the buffer management of the original printf().
+• Your function has to handle the following conversions: cspdiuxX%
+• Your function will be compared against the original printf().
+• You must use the command ar to create your library.
+    Using the libtool command is forbidden.
+• Your libftprintf.a has to be created at the root of your repository
+
+You have to implement the following conversions:
+• %c Prints a single character.
+• %s Prints a string (as defined by the common C convention).
+• %p The void * pointer argument has to be printed in hexadecimal format.
+• %d Prints a decimal (base 10) number.
+• %i Prints an integer in base 10.
+• %u Prints an unsigned decimal (base 10) number.
+• %x Prints a number in hexadecimal (base 16) lowercase format.
+• %X Prints a number in hexadecimal (base 16) uppercase format.
+• %% Prints a percent sign.
+*/
+
+#include "ft_printf.h"
+
+
+
+
+int ft_printf(const char *, ...)
+{
+    int count;
+    int lenght;
+    va_list args;
+
+    count = 0;
+	length = 0;
+	va_start(args, str);
+    while (s[count])
+    {
+        if (s[count] == '%')
+            ft_put_arg(args, s[count++])
+        else
+            ft_putchar_fd(s[count]);
+
+        count++;
+    }
+	va_end(args);
+	return (print_length);
+}
