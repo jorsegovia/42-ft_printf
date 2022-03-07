74779fd08de26a7a95a589de81d57b3aecdfecee
Jorseggil <jorge.segovia.gil@gmail.com>
2022-03-07 08:36:33 +0100

    Added functions to lib

--- ft_printf.h
+++ ft_printf.h
@@ -15,5 +15,18 @@
 
 # include "libft.h"
 # include <stdarg.h>
-
-int	ft_printf(const char *, ...);
+//printf_utils.c
+int 	ft_getlen(int n, int b);
+int		ft_printf(const char *, ...);
+//printf_char.c
+int		ft_print_char(int c);
+int		ft_print_str(char *str);
+//print_numeric.c
+int		ft_print_nbr(int n);
+char	*ft_uitoa(unsigned int n);
+int		ft_print_unsigned(unsigned int uns);
+void	ft_put_hex(unsigned int hex, const char format);
+int		ft_print_hex(unsigned int hex, const char format);
+//print_pointer.c
+void	ft_print_pointer(unsigned long ptr);
+int		ft_print_ptr(unsigned long ptr);
